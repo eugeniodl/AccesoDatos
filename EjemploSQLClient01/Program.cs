ac19ade7448bdc145be13c69ef8d07a545676e09
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-04-18 11:45:42 -0600

    EjemploSQL01

--- EjemploSQLClient01/Program.cs
+++ EjemploSQLClient01/Program.cs
@@ -2,7 +2,7 @@
 
 string connectionString = @"Data Source=DESKTOP-KQLF513\SQLSERVER2019;Initial Catalog=Northwind;Integrated Security=True";
 
-// Proporciona la cadena de consulta con un marcador de posición de parámetro.
+// Proporciona la cadena de consulta con un marcador de posición de parámetro
 string queryString = "SELECT ProductID, UnitPrice, ProductName " +
     "FROM Products WHERE UnitPrice > @pricePoint " +
     "ORDER BY UnitPrice DESC;";
@@ -10,18 +10,18 @@ string queryString = "SELECT ProductID, UnitPrice, ProductName " +
 // Especifica el valor del parámetro
 int paramValue = 30;
 
-// Crea y abre la conexión en un bloque using
-// Esto garantiza que todos los recursos se cerrarán 
+// Crea y abre la conexión en un bloque using.
+// EEsto garantiza que todos los recursos se cerrarán
 // y eliminarán cuando el código salga
 using(SqlConnection connection = new SqlConnection(connectionString))
 {
-    // Crea los objetos Commad y Parameters
+    // Crea los objetos Command y Parameters
     SqlCommand command = new SqlCommand(queryString, connection);
     command.Parameters.AddWithValue("@pricePoint", paramValue);
 
-    // Abra la conexión en un bloque try/catch.
+    // Abra la conexión en un bloque try/catch
     // Cree y ejecute el DataReader, escribiendo
-    // el conjunto de resultados en la ventana de consola.
+    // el conjunto de resultados en la ventana de la consola.
     try
     {
         connection.Open();
