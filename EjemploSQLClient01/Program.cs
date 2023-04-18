16024a7f56d6331171d723e9bb231e0ab0f53ec9
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-04-18 14:11:45 -0600

    EjemploSQLClient01

--- EjemploSQLClient01/Program.cs
+++ EjemploSQLClient01/Program.cs
@@ -1,11 +1,14 @@
-﻿using System.Data.SqlClient;
+﻿
 
-string connectionString = @"Data Source=DESKTOP-KQLF513\SQLSERVER2019;Initial Catalog=Northwind;Integrated Security=True";
+
+using System.Data.SqlClient;
+
+string connectionString = @"Data Source=DESKTOP-SSSD489\SQLSERVER2019;
+Initial Catalog=Northwind;Integrated Security=True";
 
 // Proporciona la cadena de consulta con un marcador de posición de parámetro.
-string queryString = "SELECT ProductID, UnitPrice, ProductName " +
-    "FROM Products WHERE UnitPrice > @pricePoint " +
-    "ORDER BY UnitPrice DESC;";
+string queryString = "SELECT ProductID, UnitPrice, ProductName FROM " +
+    "Products WHERE UnitPrice > @pricePoint ORDER BY UnitPrice DESC;";
 
 // Especifica el valor del parámetro
 int paramValue = 30;
@@ -19,23 +22,22 @@ using(SqlConnection connection = new SqlConnection(connectionString))
     SqlCommand command = new SqlCommand(queryString, connection);
     command.Parameters.AddWithValue("@pricePoint", paramValue);
 
-    // Abra la conexión en un bloque try/catch
+    // Abra la conexión en un bloque try/catch.
     // Cree y ejecute el DataReader, escribiendo
     // el conjunto de resultados en la ventana de la consola.
     try
     {
-
         connection.Open();
         SqlDataReader reader = command.ExecuteReader();
         while (reader.Read())
         {
-            Console.WriteLine("\t{0}\t{1}\t{2}", reader[0], reader[1], reader[2]);
+            Console.WriteLine("\t{0}\t{1}\t{2}", reader[0], reader[1],
+                reader[2]);
         }
     }
     catch (Exception ex)
     {
-
         Console.WriteLine(ex.Message);
     }
-    Console.ReadLine();
+    Console.Read();
 }
