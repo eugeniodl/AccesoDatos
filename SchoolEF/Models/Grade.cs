9a3202c9eec4c5f28f520a949bb0f6fd63a657f8
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-05-04 14:07:22 -0600

    Ejemplo EF Code First

---
+++ SchoolEF/Models/Grade.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolEF.Models
+{
+    public class Grade
+    {
+        public int Id { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+        public IList<Student> Students { get; set; }
+
+    }
+}
---
+++ SchoolEF/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolEF.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public byte[] Photo { get; set; }
+        public decimal Height { get; set; }
+        public float Weight { get; set; }
+        public int GradeId { get; set; }
+        public Grade Grade { get; set; }
+    }
+}
