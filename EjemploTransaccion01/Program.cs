350df9ed6c036939081bda367c03aade7e978c1a
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-05-10 16:05:02 -0600

    Ejemplos de Transacción

---
+++ EjemploTransaccion01/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Configuration;
+using System.Data.SqlClient;
+
+// Crear una conexión a la Base de datos
+string connectionString = ConfigurationManager.ConnectionStrings["connstring"].ToString();
+SqlConnection connection = new SqlConnection(connectionString);
+
+// Abrir la conexión
+connection.Open();
+
+// Iniciar la transacción
+SqlTransaction transaction = connection.BeginTransaction();
+
+try
+{
+    // Crear un comando para la transacción
+    SqlCommand command = connection.CreateCommand();
+    command.Transaction = transaction;
+
+    // Ejecutar una serie de comandos dentro de la transacción
+    command.CommandText = "INSERT INTO Customers(CustomerID, CompanyName, ContactName, Country) VALUES ('ALFFU', 'Alfredo Fuentes', 'Ana Mendoza', 'Nicaragua')";
+    command.ExecuteNonQuery();
+
+    command.CommandText = "INSERT INTO Orders(CustomerID, OrderDate) VALUES('ALFFU','2023-05-08')";
+    command.ExecuteNonQuery();
+
+    // Confirmar la transacción
+    transaction.Commit();
+}
+catch (Exception ex)
+{
+    // Si algo sale mal, deshacer la transacción
+    transaction.Rollback();
+    Console.WriteLine($"Error: {ex.Message}");
+}
+finally
+{
+    // Cerrar la conexión
+    connection.Close();
+}
---
+++ EjemploTransaccion02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connstring"].ToString());
+
+conn.Open();
+
+SqlTransaction trans = conn.BeginTransaction("TransferFunds");
+
+const string X_ORIGIN_ACCOUNT = "10";
+const string X_DESTINATION_ACCOUNT = "20";
+
+// Transferir fondos de una cuenta a otra (acreditar 200 de la cuenta 10 a la 20)
+decimal quantityToTransfer = 200;
+
+// Preparar consultas
+// Obtener si la cuenta 1 realmente tiene el dinero
+var sqlAccount1Fund = "SELECT (Sum(Credit) - Sum(Debit)) as Saldo FROM Accounts WHERE AccountNumber = @OriginAccount";
+
+// Consulta para extraer el valor de la cuenta de origen
+var sqlWithdrawCredit = "INSERT INTO Accounts(AccountNumber, Debit, Credit) VALUES(@OriginAccount, @Debit, 0)";
+
+// Consulta para depositar el dinero en la cuenta destino
+var sqlFundCredit = "INSERT INTO Accounts(AccountNumber, Debit, Credit) VALUES(@DestinationAccount, 0, @Credit)";
+
+try
+{
+    // Preparar los comandos que participan en la transacción
+    SqlCommand command1 = new SqlCommand(sqlAccount1Fund, conn, trans);
+    SqlCommand command2 = new SqlCommand(sqlWithdrawCredit, conn, trans);
+    SqlCommand command3 = new SqlCommand(sqlFundCredit, conn, trans);
+
+    // Leer la cuenta de origen para determinar si hay fondos.
+    command1.Parameters.AddWithValue("@OriginAccount", X_ORIGIN_ACCOUNT);
+    var funds = Convert.ToDecimal(command1.ExecuteScalar());
+    Console.WriteLine($"Total Origen Fondo Inicial: {funds:C}");
+
+    trans.Commit();
+}
+catch (Exception ex)
+{
+    trans.Rollback();
+}
+finally
+{
+    conn.Close();
+}
