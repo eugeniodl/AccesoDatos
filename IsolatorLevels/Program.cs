c35691e8b6fabbe24ead32ada41edc46245e3fdc
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2023-05-16 10:27:55 -0600

    Niveles de aislamiento

---
+++ IsolatorLevels/Program.cs
@@ -0,0 +1,17 @@
+﻿using DataLibrary;
+using System.Data;
+
+var transact1 = new TransactionOperation(10, 20, 300);
+var transact2 = new TransactionOperation(20, 30, 300);
+var t1 = transact1.ExecuteTransaction(true, 5000, IsolationLevel.ReadCommitted, "T1");
+
+// Comienza retraso de la transacción 2
+Thread.Sleep(1500);
+var t2 = transact2.ExecuteTransaction(true, 0, IsolationLevel.ReadCommitted, "T2");
+
+Task.WhenAll(t1, t2).ContinueWith(t =>
+{
+    Console.WriteLine($"Transaction1: {t1.Result} Transaction2: {t2.Result}");
+});
+
+Console.ReadLine();
