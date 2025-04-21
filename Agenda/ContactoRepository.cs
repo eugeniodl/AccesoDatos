b67d4a289e02f5e273eac9a705aa199eaa364e79
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2025-04-21 15:00:19 -0600

    GUI

--- Agenda/ContactoRepository.cs
+++ Agenda/ContactoRepository.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.Data.SqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace Agenda
 {
