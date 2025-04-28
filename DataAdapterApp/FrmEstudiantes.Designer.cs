97682be656990e16c35b9a28774e8ed4cec7d4bc
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2025-04-28 07:44:34 -0600

    Data Adapter

---
+++ DataAdapterApp/FrmEstudiantes.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DataAdapterApp
+{
+    partial class FrmEstudiantes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvEstudiantes = new DataGridView();
+            btnActualizar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvEstudiantes).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvEstudiantes
+            // 
+            dgvEstudiantes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvEstudiantes.Location = new Point(45, 28);
+            dgvEstudiantes.Name = "dgvEstudiantes";
+            dgvEstudiantes.Size = new Size(721, 346);
+            dgvEstudiantes.TabIndex = 0;
+            // 
+            // btnActualizar
+            // 
+            btnActualizar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnActualizar.Location = new Point(45, 391);
+            btnActualizar.Name = "btnActualizar";
+            btnActualizar.Size = new Size(124, 37);
+            btnActualizar.TabIndex = 1;
+            btnActualizar.Text = "Actualizar";
+            btnActualizar.UseVisualStyleBackColor = true;
+            btnActualizar.Click += btnActualizar_Click;
+            // 
+            // FrmEstudiantes
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnActualizar);
+            Controls.Add(dgvEstudiantes);
+            Name = "FrmEstudiantes";
+            Text = "FrmEstudiantes";
+            Load += FrmEstudiantes_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvEstudiantes).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvEstudiantes;
+        private Button btnActualizar;
+    }
+}
---
+++ DataAdapterApp/FrmEstudiantes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataAdapterApp
+{
+    public partial class FrmEstudiantes : Form
+    {
+        private readonly IStudentsRepository _studentsRepository;
+        private DataSet dataSet;
+
+        public FrmEstudiantes()
+        {
+            InitializeComponent();
+            string connectionString =
+                ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
+            _studentsRepository = new StudentsRepository(connectionString);
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _studentsRepository.UpdateStudentsDataSet(dataSet);
+                MessageBox.Show("Estuadiantes actualizados.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al actualizar los estudiantes: {ex.Message}");
+            }
+        }
+
+        private void FrmEstudiantes_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                dataSet = _studentsRepository.GetStudentsDataSet();
+                dgvEstudiantes.DataSource = dataSet;
+                dgvEstudiantes.DataMember = "Estudiantes";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al obtener los estudiantes: {ex.Message}");
+            }
+        }
+    }
+}
