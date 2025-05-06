dff7139b406c7cef298e4ee75da08991225a7e42
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2025-05-06 11:13:32 -0600

    Manipulación de datos EF

--- EjemploEF01/Program.cs
+++ EjemploEF01/Program.cs
@@ -23,4 +23,59 @@ using (var db = new NorthwindContext())
         Console.WriteLine($"Producto = {producto.ProductName}, " +
             $"Precio = {producto.UnitPrice}");
     }
+
+    // 35. Empleados con cargo 'Sales Representative'
+
+    var empleadoVentas = db.Employees
+        .Where(e => e.Title == "Sales Representative")
+        .ToList();
+
+    Console.WriteLine("Empleados con cargo 'Sales Representative'");
+    foreach (var empleado in empleadoVentas)
+    {
+        Console.WriteLine($"{empleado.FirstName} " +
+            $"{empleado.LastName}, {empleado.Title}");
+    }
+
+    // 5. Productos y su categoría
+    var productosConCategoria = db.Products
+        .Select(p => new {
+            p.ProductName,
+            Categoria = p.Category.CategoryName
+        })
+        .ToList();
+
+    // 8. Primer pedido de cada cliente
+    var primerPedidoCliente = db.Orders
+        .GroupBy(o => o.CustomerId)
+        .Select(g => g.OrderBy(o => o.OrderDate)
+        .FirstOrDefault())
+        .ToList();
+
+
+    // 17. Suma de ventas por cliente
+    var ventasPorCliente = db.Orders
+        .Where(o => o.OrderDetails.Any())
+        .GroupBy(o => o.CustomerId)
+        .Select(g => new
+        {
+            ClienteID = g.Key,
+            TotalVentas = g.Sum(o => o.OrderDetails
+            .Sum(d => d.Quantity * d.UnitPrice))
+        }).ToList();
+
+    // 21. Productos descontinuados
+    var productosDescontinuados = db.Products
+        .Where(p => p.Discontinued).ToList();
+
+    // 28. Órdenes que fueron enviadas tarde (ShippedDate > RequiredDate) 
+    var ordenesTarde = db.Orders
+        .Where(o => o.ShippedDate.HasValue
+        && o.RequiredDate.HasValue
+        && o.ShippedDate > o.RequiredDate).ToList();
+
+    // 24. Empleados que reportan directamente al jefe con ID 2 
+    var empleadosReportan2 = db.Employees
+        .Where(e => e.ReportsTo == 2).ToList();
+
 }
---
+++ EjemploEF02/Data/LibraryContext.cs
@@ -0,0 +1,26 @@
+﻿using EjemploEF02.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploEF02.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection =
+                ConfigurationManager
+                .ConnectionStrings["constring"].ConnectionString;
+            optionsBuilder.UseSqlServer(connection);
+        }
+    }
+}
