f029aa210c6fa7ef79fd05ae7f01829f233a651c
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2025-05-13 10:20:58 -0600

    Transacciones

---
+++ EjemploTransacciones01/Data/BankContext.cs
@@ -0,0 +1,36 @@
+﻿using EjemploTransacciones01.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploTransacciones01.Data
+{
+    public class BankContext : DbContext
+    {
+        public DbSet<AccountTransaction> AccountTransactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection =
+                ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
+            optionsBuilder.UseSqlServer(connection);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AccountTransaction>().HasData(
+                new AccountTransaction()
+                {
+                    Id = 1,
+                    AccountNumber = "10",
+                    Credit = 400M,
+                    Debit = 0M
+                });
+        }
+    }
+}
---
+++ EjemploTransacciones01/Models/AccountTransaction.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploTransacciones01.Models
+{
+    public class AccountTransaction
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string AccountNumber { get; set; }
+        [Precision(18, 2)]
+        public decimal Credit { get; set; }
+        [Precision(18, 2)]
+        public decimal Debit { get; set; }
+    }
+}
